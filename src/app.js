1ba51b7e883545f74d9f2da9cdd604c635c580ff
Luis Matias Belderrain <Matibelde@hotmail.com>
2025-03-28 18:18:19 +0000

    boton  y generar carta

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+ document.getElementById("mostrar").addEventListener("click",generarCarta)
 };
+
+function generarCarta() {
+  let palos = ["♦", "♥", "♠", "♣"]
+  let palosRandom = palos[Math.floor(Math.random() * palos.length)]
+  // console.log(palosRandom)
+  let numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"]
+  let numerosRandom = numeros[Math.floor(Math.random() * numeros.length)]
+
+  let cantidadCartas = document.getElementById("cantidad").value
+  console.log(cantidadCartas)
+}
